57a6e555bd8bea8773d99079e887f882c7fbe0e3
arjun616 <arjunkrishnadas4@gmail.com>
2022-09-21 12:18:55 +0530

    shopping

---
+++ shopping-cart/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SiteLayoutModule } from './site-layout/site-layout.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SiteLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
